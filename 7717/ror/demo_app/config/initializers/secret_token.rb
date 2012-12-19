2350530c03f07c43344e1fd593e9453d1a775b98
ssharma <ssharma@verisys.com>
2012-12-19 01:19:19 -0700

    hw9

---
+++ 7717/ror/demo_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '9565b525d4d54f81d6b34d418feb1bcd01faa2a54849d6a76f8b1c802e45ae5963618945695500c2a29e5f19ec21e8e2295079eae639eb48a2c693a9c596548c'
